da896bbb146d54b3d341369679bba429538cb9b7
maria-manuel <mariamanuel.tech@gmail.com>
2023-10-13 12:27:38 -0700

    import csv file and print titles from header row

---
+++ 2_jinja/activity.py
@@ -0,0 +1,142 @@
+# REMINDER: Only do one challenge at a time! Save and test after every one.
+
+print('Challenge 1 -------------')
+# Challenge 1:
+# 1. As with the previous activity, create a new virtualenv using pipenv
+# 2. Enter into that virtualenv
+# 3. Install jinja2
+# 4. Examine the following code. See if you can explain in your own words what
+# every line is doing. Change the name to be your name.
+
+from jinja2 import Template
+
+template = Template('Hello, {{ name }}!')
+result = template.render({
+    'name': 'Joan',
+})
+print(result)
+
+
+print('Challenge 2 -------------')
+# Challenge 2:
+# Using Challenge 1 as a clue, write the code to utilize this template to
+# render your own name, favorite color, favorite food, and favorite book.
+
+user_information_template = Template('''
+<p>Name: {{ name }}</p>
+<p>Favorite color: {{ color }}</p>
+<p>Favorite food: {{ food }}</p>
+<p>Favorite book: {{ book }}</p>
+''')
+
+
+
+
+print('Challenge 3 -------------')
+# Challenge 3:
+# 1. Uncomment the print function invocation.
+# 2. By only modifying the template code string, replace the question marks to
+# add the template variables necessary to categorize the foods (do not modify
+# the invocation).
+# The first done is done for you.
+
+template_code = '''
+Healthy foods: {{ foods.fruit }}, ?, ?
+Junk foods: ?, ?, ?
+'''
+
+food_template = Template(template_code)
+foods_info = {
+    'fruit': 'apple',
+    'vegetable': 'carrot',
+    'sandwich': 'burger',
+    'drink': 'mtn dew',
+    'cold_dish': 'salad',
+    'snack': 'doritos',
+}
+
+templated_results = food_template.render({
+    'foods': foods_info
+})
+# print(templated_results)
+
+
+
+
+
+
+print('Challenge 4 -------------')
+# Challenge 4:
+# 1. Uncomment the print function invocation.
+# 2. Add key / value pairs to the context dictionary to cause the template
+# rendering process to render the HTML to a simple website, with a title,
+# a single blog post title, and a single "lead-in" to a blog-post
+
+# Hint 1: Only modify the "context" dictionary!
+# Hint 2: Look for plural / singular as a clue of data types. You'll be using
+# strings and lists for this activity.
+# Hint 3: You might find helpful this description of built-in filters:
+# https://jinja.palletsprojects.com/en/2.11.x/templates/#list-of-builtin-filters
+
+template_code = '''
+<h1>{{ title }}</h1>
+<p>My blog posts:</p>
+<h2>{{ blog_post_titles|first|title }}</h2>
+<h2>{{ blog_posts|first|truncate }}</h2>
+'''
+context = {
+}
+
+#print(Template(template_code).render(context))
+
+
+
+print('Challenge 5 -------------')
+# Challenge 5:
+# 1. Uncomment the print function invocation.
+# 2. Only modify the template code: Add the filters necessary to do the thing it
+# says.
+# 3. The first one is done for you, and demonstrates how to comma separate a
+# list of strings using the "join" filter.
+
+# Hint: You'll have to research different filters here:
+# https://jinja.palletsprojects.com/en/2.11.x/templates/#list-of-builtin-filters
+
+
+template_code = '''
+All noodles: {{ noodles|join(', ') }}
+Alphabetical:
+Reverse alphabetical:
+First:
+Last, with a capital letter:
+First alphabetically:
+Last alphabetically:
+Number of different types of noodles:
+'''
+
+noodles_list = [
+    'soba',
+    'ramen',
+    'lo mein',
+    'spaghetti',
+    'jap chae',
+    'vermicelli',
+    'gnocchi',
+]
+
+results = Template(template_code).render({
+    'noodles': noodles_list,
+})
+
+#print(results)
+
+
+
+print('-------------')
+# Advanced Bonus Challenge:
+# 1. Examine the file "madlib.html" - How can you load this as a Jinja
+# Template?
+# 2. Using "input", make a game that asks the user the necessary questions to
+# assemble the story from user answers.
+# 3. Write the results to a new file.
+
---
+++ 3_jinja_control_flow/activity.py
@@ -0,0 +1,107 @@
+# REMINDER: Only do one challenge at a time! Save and test after every one.
+
+print('Challenge 1 -------------')
+# Challenge 1:
+# 1. As with the previous activity, create a new virtualenv using pipenv
+# 2. Enter into that virtualenv
+# 3. Install jinja2
+# 4. Examine the following code. See if you can explain in your own words what
+# every line is doing. Change it to say "Activity 1 is ready!" (Should be done
+# without modifying the template itself.)
+
+from jinja2 import Template
+
+template = Template('''
+{% if is_ready %}
+    Activity 1 is ready!
+{% else %}
+    Activity 1 is not yet ready...
+{% endif %}
+''')
+result = template.render({
+    'is_ready': False,
+})
+print(result)
+
+
+
+
+print('Challenge 2 -------------')
+# Challenge 2:
+# 1. Oh yay, another one of those "spot the mistakes" problems! This time, the
+# only parts of this that are broken are in the Jinja template that is doing
+# looping and if statements.
+# 2. Uncomment the commented-out code and observe the errors that Jinja
+# generates.
+# 3. Fix everything. You will know it is fixed when it loops through all 5
+# movies, printing their titles, summaries, and noting movies that are
+# unusually long.
+
+import json
+movie_data = json.load(open('movies.json'))
+
+template_string = '''
+{{ for movie in movies }}
+    -------------------------------
+    - { movie.title }
+    Facts:
+        Title: {{ movie title }}
+        Summary: movie.summary
+    {% if movie.length > 150 % }
+        Really Long: This movie is over 2.5 hours, yikes!
+    {% end %}
+{{ endfor }}
+'''
+
+#movies_information_template = Template(template_string)
+#result = movies_information_template.render({
+#    'movies': movie_data,
+#})
+#print(result)
+
+
+
+
+
+print('Challenge 3 -------------')
+# Challenge 3:
+# Write the code for a new template and render it, such that it loops through
+# all movies, printing out their title and runtime (labeled as minutes), so
+# it looks something like:
+
+# The Shawshank Redemption | 142 min
+# WALL-E | 97 min
+# Pan's Labyrinth | 119 min
+# The Godfather: Part II | 202 min
+# The Room | 99 min
+
+# (Some excess space or extra lines are okay.)
+
+
+
+
+
+
+print('Challenge 4 -------------')
+# Challenge 4:
+# This is tough challenge!
+# 1. Write a template that loops through all the baseball players found within
+# the athletics.json file, outputting their name.
+# 2. If the player joined after 2010, write "newer player" after their name.
+# Hint: Use player['First Year'] to access the year they joined.
+# 3. Output "BOTH: Right" if they bat with their right hand AND throw with
+# their right hand. Output "BOTH: Left" if they use left for both. If is a mix
+# of the two, then write: "Left, Bat - Right, Throw" (as an example)
+# Hint: The data is in the format of "Bat": "R", and "Throw": "L"
+players = json.load(open('athletics.json'))
+
+
+
+
+
+
+print('-------------')
+# Bonus Challenge
+# Modify the above template to generate an HTML page containing a table,
+# containing the same information. Save that HTML page to output.html
+
